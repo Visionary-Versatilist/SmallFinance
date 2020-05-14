634304c8de513d2bc82adf6c62565145e10a56af
Visionary-Versatilist <rk.nallakumar@outlook.com>
2020-05-14 21:54:15 +0530

    first commit for testing

--- src/components/CollectionReports/CollectionReport.js
+++ src/components/CollectionReports/CollectionReport.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import Sidebar from '../sidebar/sidebar';
+// import React, { Component } from 'react';
+// import Sidebar from '../sidebar/sidebar';
 
-class CollectionReport extends Component {
-    render(){
-        return(
-            <div>
-            <Sidebar/>
-            <div style={{marginLeft: '50%',marginTop: '18%',}}>
-            Collection Report Page is Working! 
-            </div>
-            </div>
-        )
-    }
-}
-export default CollectionReport;
+// class CollectionReport extends Component {
+//     render(){
+//         return(
+//             <div>
+//             <Sidebar/>
+//             <div style={{marginLeft: '50%',marginTop: '18%',}}>
+//             Collection Report Page is Working! 
+//             </div>
+//             </div>
+//         )
+//     }
+// }
+// export default CollectionReport;
